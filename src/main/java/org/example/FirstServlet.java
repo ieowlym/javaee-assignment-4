aceb02177970b7f1a958bb643b2c46f72d2fc00c
ieowlym <>
2021-02-22 12:19:16 +0600

    url-rewriting

--- src/main/java/org/example/FirstServlet.java
+++ src/main/java/org/example/FirstServlet.java
@@ -5,8 +5,9 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet(value = "/hidden-form-field-in-session-tracking/servlet1")
+@WebServlet(value = "/servlet1")
 public class FirstServlet extends HttpServlet {
+
     public void doGet(HttpServletRequest request, HttpServletResponse response){
         try{
 
@@ -16,11 +17,9 @@ public class FirstServlet extends HttpServlet {
             String n=request.getParameter("userName");
             out.print("Welcome "+n);
 
-            //creating form that have invisible textfield
-            out.print("<form action='servlet2'>");
-            out.print("<input type='hidden' name='uname' value='"+n+"'>");
-            out.print("<input type='submit' value='go'>");
-            out.print("</form>");
+            //appending the username in the query string
+            out.print("<br><a href='servlet2?uname="+n+"'>visit</a>");
+
             out.close();
 
         }catch(Exception e){System.out.println(e);}
--- src/main/java/org/example/SecondServlet.java
+++ src/main/java/org/example/SecondServlet.java
@@ -5,18 +5,23 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet(value = "/hidden-form-field-in-session-tracking/servlet2")
+@WebServlet(value = "/servlet2")
 public class SecondServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)  {
         try{
+
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        //Getting the value from the hidden field
+        //getting value from the query string
         String n=request.getParameter("uname");
         out.print("Hello "+n);
 
         out.close();
+
     }catch(Exception e){System.out.println(e);}
 }
+
+
 }
